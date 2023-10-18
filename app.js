5ae8dde3ccc4ff872addf206f03fb278dd4c299f
SneakyMeligy <sherifkayed94@gmail.com>
2023-10-18 10:58:48 +0200

    Make sure latest response is displayed matches latest request

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ function isValidURL(url) {
 const inputElem = document.getElementById('urlInput');
 const messageElem = document.getElementById('message');
 
+let latestRequestId = null;
+
 const handleInputChange = _.debounce(async () => {
     const url = inputElem.value;
 
@@ -27,15 +29,21 @@ const handleInputChange = _.debounce(async () => {
         return;
     }
 
+    // Generate a unique identifier for the request using timestamp
+    const currentRequestId = Date.now();
+    latestRequestId = currentRequestId;
+
     messageElem.textContent = "Checking...";
     const { exists, type } = await checkURLExists(url);
 
-    if (exists) {
-        messageElem.textContent = `URL exists and it is a ${type}.`;
-    } else {
-        messageElem.textContent = "URL does not exist.";
+    // Only update the UI if the current request Id is equal to the latest one
+    if (latestRequestId === currentRequestId) {
+        if (exists) {
+            messageElem.textContent = `URL exists and it is a ${type}.`;
+        } else {
+            messageElem.textContent = "URL does not exist.";
+        }
     }
-
 }, 500);  // Throttle the server request to every 500ms
 
 inputElem.addEventListener('input', handleInputChange);
